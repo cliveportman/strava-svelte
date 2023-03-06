6386f4cc13856a17bd876dcfaa9e870d1a3e6fc4
Clive Portman <clive@theportman.co>
2023-03-06 17:49:05 +0000

    adding host to env vars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  // import the stores
-  import { authStore, stravaStore } from "$stores/strava-store";
+  const host = import.meta.env.VITE_HOST;
+  const url = `https://www.strava.com/oauth/authorize?client_id=17771&response_type=code&redirect_uri=${host}auth/token&approval_prompt=force&scope=read,activity:read`;
 </script>
 
-<a
-  href="https://www.strava.com/oauth/authorize?client_id=17771&response_type=code&redirect_uri=http://127.0.0.1:5173/auth/token&approval_prompt=force&scope=read,activity:read"
-  >Authenticate with your Strava account</a
->
+<div class=" flex justify-center p-4">
+  <a class="m-2 p-4 bg-orange-500 text-white" href={url}
+    >Authenticate with your Strava account</a
+  >
+</div>
